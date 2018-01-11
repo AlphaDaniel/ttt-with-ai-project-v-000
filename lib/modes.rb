fc6d42c7a51bfd0c7853ba046ff8c87892bcdf2d
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 14:16:57 +0000

    automatically backed up by learn

--- lib/modes.rb
+++ lib/modes.rb
@@ -9,18 +9,18 @@ module Modes
     Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
   end
 
-  # def Spectator
-  #   self.mode = "valid"
-  #   puts "COMPUTER vs COMPUTER... GAME STARTING IN 3..2..1!".cyan
-  #   sleep(2)
-  #   Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
-  # end
-  #
-  # def Spectator
-  #   self.mode = "valid"
-  #   puts "COMPUTER vs COMPUTER... GAME STARTING IN 3..2..1!".cyan
-  #   sleep(2)
-  #   Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
-  # end
+  def vs_computer
+    self.mode = "valid"
+    puts "YOU vs COMPUTER... GOOD LUCK!".cyan
+    sleep(2)
+    Game.new(Players::Human.new("X"), Players::Computer.new("O")).play
+  end
+
+  def player_vs_player
+    self.mode = "valid"
+    puts "COMPUTER vs COMPUTER... GAME STARTING IN 3..2..1!".cyan
+    sleep(2)
+    Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
+  end
 #==============================================
 end
