a8f3876df560c0be3889f0084547fd68d3eff5db
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 12:47:09 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -21,7 +21,12 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
   def over?
     won? || draw?
   end
-
+#====================================game_states======================================
+#=====================================================================================
+  def current_player
+    board.turn_count.even? ? player_1 : player_2
+  end
+  
   def winner
     win_combo_char unless !won?
   end
