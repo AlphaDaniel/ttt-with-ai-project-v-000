7cc895b5c8f112298b898ad8042b469e8f19f7a8
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 13:07:34 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -33,19 +33,19 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
 
 
 
-  def turn
-    move = current_player.move(board)
-    sleep(0.3)
-    board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? turn : error!
-    board.display
-    puts "==========="
-  end
-
-  def error!
-    puts "\n Invalid Move Try Again \n".colorize(color: :white, background: :red)
-    sleep(0.2)
-    turn
-  end
+  # def turn
+  #   move = current_player.move(board)
+  #   sleep(0.3)
+  #   board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? turn : error!
+  #   board.display
+  #   puts "==========="
+  # end
+  #
+  # def error!
+  #   puts "\n Invalid Move Try Again \n".colorize(color: :white, background: :red)
+  #   sleep(0.2)
+  #   turn
+  # end
 #========================================CLI==========================================
   def self.start
     puts "Welcome To Tic Tac Toe!".cyan
