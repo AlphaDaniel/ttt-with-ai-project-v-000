9b6afa830d2d55338bf9e22cd9bc0f42de40f204
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 12:55:42 +0000

    automatically backed up by learn

--- lib/player.rb
+++ lib/player.rb
@@ -9,7 +9,7 @@ class Player
   def self.turn
     move = current_player.move(board)
     sleep(0.3)
-    board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? turn : error!
+    board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? self.turn : error!
     board.display
     puts "==========="
   end
@@ -17,7 +17,7 @@ class Player
   def self.error!
     puts "\n Invalid Move Try Again \n".colorize(color: :white, background: :red)
     sleep(0.2)
-    turn
+    self.turn
   end
 #==============================================
 end
