32f466842736569b0631f91f61d4552ee4d8f80e
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 13:35:04 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -26,7 +26,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
   # def current_player
   #   board.turn_count.even? ? player_1 : player_2
   # end
-  # 
+  #
   # def turn
   #   move = current_player.move(board)
   #   sleep(0.3)
@@ -34,13 +34,13 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
   #   board.display
   #   puts "==========="
   # end
-  # 
+  #
   # def error!
   #   puts "\n Invalid Move Try Again \n".colorize(color: :white, background: :red)
   #   sleep(0.2)
   #   turn
   # end
-  # 
+  #
   # def winner
   #   win_combo_char unless !won?
   # end
