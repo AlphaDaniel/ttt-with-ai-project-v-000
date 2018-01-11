8e30dd1388f0c2adae12e048ac3069bd53a1238d
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 13:38:12 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -9,26 +9,4 @@ module Players
     end
   end
 #==============================================
-  def current_player
-    board.turn_count.even? ? player_1 : player_2
-  end
-
-  def turn
-    move = current_player.move(board)
-    sleep(0.3)
-    board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? turn : error!
-    board.display
-    puts "==========="
-  end
-
-  def error!
-    puts "\n Invalid Move Try Again \n".colorize(color: :white, background: :red)
-    sleep(0.2)
-    turn
-  end
-
-  def winner
-    win_combo_char unless !won?
-  end
-#==============================================
 end
