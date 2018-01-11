1382e6db5e7eadfb079b933df7ad7c6ee911eeb3
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 14:13:23 +0000

    automatically backed up by learn

--- lib/modes.rb
+++ lib/modes.rb
@@ -15,7 +15,7 @@ module Modes
   #   sleep(2)
   #   Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
   # end
-  # 
+  #
   # def Spectator
   #   self.mode = "valid"
   #   puts "COMPUTER vs COMPUTER... GAME STARTING IN 3..2..1!".cyan
