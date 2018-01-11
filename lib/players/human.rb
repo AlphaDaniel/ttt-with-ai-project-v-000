8fa4a05e941ce884143727304baa20da3b1b8f9c
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 11:56:04 +0000

    automatically backed up by learn

---
+++ lib/players/human.rb
@@ -0,0 +1,11 @@
+module Players
+#====================human=====================
+  class Human < Player
+    def move(board)
+      board.display
+      puts "Your Move #{self.token}! (Enter 1-9)".yellow
+      gets.strip
+    end
+  end
+#==============================================
+end 
