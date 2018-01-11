fca8336f1f00ed100e7e6b9ce564b6848429333b
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 15:59:02 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -61,7 +61,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
     case ans
     when "y"
       Game.start
-    # when "n"
+    when "n"
     #   vs_computer_start
     #   Game.new(Players::Computer.new("O"), Players::Human.new("X")).play
     # else
