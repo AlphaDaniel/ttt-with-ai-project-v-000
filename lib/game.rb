2c5f3166a35ebb34adce3c10ef77115e87a2be7d
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 14:44:54 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -32,7 +32,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
   end
 
   def self.choose
-    mode = gets.strip
+    self.mode = gets.strip
     until self.mode == "valid"
       if self.mode == "0"
         self.spectator
