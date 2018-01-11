9618581567e7a2b5cd7c0dbf2d9564305713cf06
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 13:19:04 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,5 +1,18 @@
 # Initialize Players::Human.new("X" or "O")
 module Players
+  def self.turn
+    move = current_player.move(board)
+    sleep(0.3)
+    board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? turn : error!
+    board.display
+    puts "==========="
+  end
+
+  def self.error!
+    puts "\n Invalid Move Try Again \n".colorize(color: :white, background: :red)
+    sleep(0.2)
+    turn
+  end
 #====================human=====================
   class Human < Player
     def move(board)
