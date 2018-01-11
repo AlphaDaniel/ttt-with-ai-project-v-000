623b968b8e6a401b4ed572c6065950c6bb69943a
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 13:20:29 +0000

    automatically backed up by learn

--- lib/player.rb
+++ lib/player.rb
@@ -5,5 +5,19 @@ class Player
   def initialize(char)
     @token = char
   end
+
+  def self.turn
+    move = current_player.move(board)
+    sleep(0.3)
+    board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? turn : error!
+    board.display
+    puts "==========="
+  end
+
+  def self.error!
+    puts "\n Invalid Move Try Again \n".colorize(color: :white, background: :red)
+    sleep(0.2)
+    turn
+  end
 #==============================================
 end
