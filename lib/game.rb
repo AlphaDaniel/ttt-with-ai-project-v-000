04844826802e1687d9250995d92bfd049907ed1e
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 14:32:04 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -41,7 +41,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
       elsif self.mode == "2"
         self.player_vs_player
       else
-        self.mode_error
+        self.mode_error!
       end
     end
   end
