48c6afb9605358d32c9e75e6e94de019bad0b3b2
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 14:13:21 +0000

    automatically backed up by learn

--- lib/modes.rb
+++ lib/modes.rb
@@ -2,11 +2,25 @@ module Modes
 #=================properties===================
   attr_accessor :mode
 #==============================================
-  def Spectator
+  def spectator
     self.mode = "valid"
     puts "COMPUTER vs COMPUTER... GAME STARTING IN 3..2..1!".cyan
     sleep(2)
     Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
   end
+
+  # def Spectator
+  #   self.mode = "valid"
+  #   puts "COMPUTER vs COMPUTER... GAME STARTING IN 3..2..1!".cyan
+  #   sleep(2)
+  #   Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
+  # end
+  # 
+  # def Spectator
+  #   self.mode = "valid"
+  #   puts "COMPUTER vs COMPUTER... GAME STARTING IN 3..2..1!".cyan
+  #   sleep(2)
+  #   Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
+  # end
 #==============================================
 end
