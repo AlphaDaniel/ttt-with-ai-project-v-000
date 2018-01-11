cf67a0d912906c3da92a0731e86d069fdb00e76d
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 16:03:12 +0000

    automatically backed up by learn

--- lib/modes.rb
+++ lib/modes.rb
@@ -1,14 +1,14 @@
 module Modes
 #=================properties===================
   attr_accessor :mode
-#==============================================
+#==================spectator===================
   def spectator
     self.mode = "valid"
     puts "COMPUTER vs COMPUTER... GAME STARTING IN 3..2..1!".cyan
     sleep(2)
     Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
   end
-#==============================================
+#================vs_computer===================
   def vs_computer
     self.mode = "valid"
     puts "YOU vs COMPUTER... WOULD YOU LIKE TO GO FIRST?".cyan
@@ -30,24 +30,24 @@ module Modes
     puts "GAME STARTING... GOOD LUCK!".cyan
     sleep(2)
   end
-
-  def yes_no_error!
-    puts "\n Invalid Entry Please Enter y or n \n".colorize(color: :white, background: :red)
-    sleep(0.3)
-    self.vs_computer
-  end
-#==============================================
+#==============player_vs_player================
   def player_vs_player
     self.mode = "valid"
     puts "PLAYER vs PLAYER... MAY THE BEST MAN WIN!".cyan
     sleep(2.5)
     Game.new.play
   end
-
+#===================errors=====================
   def mode_error!
     puts "\n Invalid Entry Please Enter 0, 1, or 2 \n".colorize(color: :white, background: :red)
     sleep(0.3)
     self.start
+  end
+
+  def yes_no_error!
+    puts "\n Invalid Entry Please Enter y or n \n".colorize(color: :white, background: :red)
+    sleep(0.3)
+    self.vs_computer
   end
 #==============================================
 end
