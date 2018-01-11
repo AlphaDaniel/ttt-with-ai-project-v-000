951f2f8f61b917622632eb7011a5d918934c7488
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 14:24:44 +0000

    automatically backed up by learn

--- lib/players/all_players.rb
+++ lib/players/all_players.rb
@@ -7,12 +7,12 @@ module Players
   def turn
     move = current_player.move(board)
     sleep(0.3)
-    board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? turn : error!
+    board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? turn : move_error!
     board.display
     puts "==========="
   end
 
-  def error!
+  def move_error!
     puts "\n Invalid Move Try Again \n".colorize(color: :white, background: :red)
     sleep(0.2)
     turn
