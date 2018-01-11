47eb757dafb50df8551612d8668608d9d4fca4e5
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 13:40:17 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -22,28 +22,6 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
   def over?
     won? || draw?
   end
-#======================================player=========================================
-  # def current_player
-  #   board.turn_count.even? ? player_1 : player_2
-  # end
-  #
-  # def turn
-  #   move = current_player.move(board)
-  #   sleep(0.3)
-  #   board.valid_move?(move)? board.update(move, current_player) : current_player.class == Players::Computer ? turn : error!
-  #   board.display
-  #   puts "==========="
-  # end
-  #
-  # def error!
-  #   puts "\n Invalid Move Try Again \n".colorize(color: :white, background: :red)
-  #   sleep(0.2)
-  #   turn
-  # end
-  #
-  # def winner
-  #   win_combo_char unless !won?
-  # end
 #========================================CLI==========================================
   def self.start
     puts "Welcome To Tic Tac Toe!".cyan
