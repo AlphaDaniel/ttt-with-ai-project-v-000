2b252de055b0a7ff05f7cb3924d297e681ac54d0
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-11 12:03:40 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,6 @@ module Players
 #======================AI======================
   class Computer < Player
     def move(board)
-      # puts "==========="
       sleep(0.5)
       puts "AI is thinking...".red
       rand(1..9).to_s
